d4b0cfeb759bad69b99db03dc5a8e16e79e0c5ed
dongkookAn <97921464+dongkookAn@users.noreply.github.com>
2022-01-19 20:42:29 +0900

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner (System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		sc.close();
+		
+		if (a>=1) {
+			
+		if (b>=45) {
+			System.out.print(a);
+			System.out.print(b-45);}
+			
+				
+			
+		if (b<45) {
+			System.out.print(a-1);	
+			System.out.print(" "60-(45-b));		
+		}
+		
+		}
+		if (a<1) {
+			if(b<45) {
+				System.out.print(23);
+				System.out.print(" "60-(45-b));
+				
+			}
+		}
+		}
+		}
+	
+		
+	
+
